bf570c6ad517d944a51571a541dd519537997be3
Mahdi Dibaiee <mdibaiee@aol.com>
2018-01-14 14:54:25 +0330

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'humandate',
     packages = ['humandate'],
-    version = '0.4',
+    version = '0.5',
     description = 'Parse human-readable dates',
     author = 'Mahdi Dibaiee',
     author_email = 'mdibaiee@aol.com',
