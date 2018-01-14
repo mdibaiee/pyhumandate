0f60c139a986f66844c72409e5aec16140e16e3a
Mahdi Dibaiee <mdibaiee@aol.com>
2018-01-14 14:29:46 +0330

    pypi package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'humandate',
+    packages = ['humandate'],
+    version = '0.1',
+    description = 'Parse human-readable dates',
+    author = 'Mahdi Dibaiee',
+    author_email = 'mdibaiee@aol.com',
+    url = 'https://github.com/mdibaiee/pyhumandate',
+    download_url = 'https://github.com/mdibaiee/pyhumandate/archive/master.tar.gz',
+    keywords = ['date', 'parsing', 'parser', 'time', 'datetime', 'human'],
+    classifiers = [],
+)
